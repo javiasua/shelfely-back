b51e35323580b7c13a12cd67cfd0539c4bd730c2
Javier Asua <asuajavier@gmail.com>
2020-07-03 10:21:31 +0200

    DONE

--- routes/file-upload.routes.js
+++ routes/file-upload.routes.js
@@ -6,27 +6,19 @@ const uploader = require('../config/cloudinary.config.js');
 
 
 
-let middleware = (req, res, next) => {
-    console.log(req.file)
+
+
+router.post('/upload',uploader.single('imageUrl') , (req, res, next) => {
+     console.log('file is: ', req.file)
     if (!req.file) {
         res.json({
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'
         })
     }
     else {
-     uploader.single('imageUrl')  
-     next() 
+        res.json({ image: req.file.path });
     }
     
-}
-
-router.post('/upload', middleware, (req, res, next) => {
-     console.log('file is: ', req.file)
-    if (!req.file) {
-      res.json({error: 'No image uplaoded'})
-    }
-
-    res.json({ image: req.file.path });
 })
 
 module.exports = router;
